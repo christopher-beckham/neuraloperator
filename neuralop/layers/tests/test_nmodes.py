4c3301313b89485c16f58e802d0e11cb1dd96bf2
Christopher Beckham <christopher.j.beckham@gmail.com>
2023-09-28 14:57:37 -0400

    add test for n modes

---
+++ neuralop/layers/tests/test_nmodes.py
@@ -0,0 +1,34 @@
+import torch
+
+from neuralop.layers.fno_block import FNOBlocks
+
+def test_fnoblock_nmodes():
+
+    sdim = 32
+    xfake = torch.randn((4, 8, sdim, sdim))
+
+    block1 = FNOBlocks(
+        in_channels=8,
+        out_channels=16,
+        n_modes=(35,35),
+        n_layers=1
+    )
+
+    print("Run block1:")
+    block1(xfake)
+
+    print("----")
+
+    block2 = FNOBlocks(
+        in_channels=8,
+        out_channels=16,
+        n_modes=(36,36),
+        n_layers=1
+    )
+
+    print("Run block2:")
+    block2(xfake)
+
+    
+if __name__ == '__main__':
+    test_fnoblock_nmodes()
